4e17103dc75f5a4b9c994facf423911dc8617737
Sugam Poudel <sugampoudel528@gmail.com>
2026-05-01 11:29:30 +0200

    String Sliceing, unicode, type conversion, truthy and falsy values

---
+++ revision01/Strings.py
@@ -0,0 +1,7 @@
+# Every String has a unicode whihc u can see by using ord(but for only one carater) chr is used to convert from unicode to string 
+greetings = "H"
+print(ord(greetings))
+print(chr(72))
+b = "Hello"
+print(b[0:3:1])
+print(b[::])
