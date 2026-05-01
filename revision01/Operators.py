ac0c245b540ecec4255a6ed0f656c567ec017035
Sugam Poudel <sugampoudel528@gmail.com>
2026-05-01 11:57:05 +0200

    Oprators
    Oprators, compund oprators, comparisions and ASCII value

---
+++ revision01/Operators.py
@@ -0,0 +1,39 @@
+a = 12
+b = 30 
+print(a+b)
+print(b-a)
+print(a*b)
+print(b/a)
+print(b%a)
+print(b**a)
+print(b//a) #py also follows BODMAS Rule for math calculation
+
+# compund arthematic oprations
+a += b
+print(a)
+
+a -=b
+print(a)
+
+a *=b
+print(a)
+
+a /=b
+print(a)
+
+a //=b
+print(a)
+
+a **=b
+print(a)
+print(a)
+
+
+# comparision
+
+# ==
+# !=
+# ><
+# >=
+# <=
+# u can also compare value using ASCII value !!
