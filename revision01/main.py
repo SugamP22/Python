69909de43bdcbf06be10af059c09c81a4d6d12e7
Sugam Poudel <sugampoudel528@gmail.com>
2026-05-01 10:21:05 +0200

    Create main.py
    Data types

---
+++ revision01/main.py
@@ -0,0 +1,15 @@
+# Variables
+Name = "Sugam" #this is a string data type
+CurrentCondition = "Intership at HISPASEC" # this is a string data typr
+OverallTime = 450 # this is a integer data type
+# what are thing sthat are sotre in the variable
+# basically data are store in the variable 
+# Thress main data type which is Numbers, String , Boolean
+a = -23 # this is integer
+print(type(a))
+# Interesting thing about python is that anything in the form of p/q is always a float type 
+b = 1/2 #this is float
+b = 23.32 # this is float
+c = 34j # this is a complex data type
+d = True # this is a boolean data type
+# Every String has a unicode whihc u can see by using ord chr is used to convert from unicode to string 
